039166a56598cef602f04260aa7cab6696c1b848
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-16 11:28:23 -0500

    working POST request

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,12 +1,30 @@
+const { query } = require("express");
 const express = require("express");
 const koalaRouter = express.Router();
-const pool = require("../modules/pool");
-
 // DB CONNECTION
+const pool = require("../modules/pool");
 
 // GET
 
 // POST
+
+koalaRouter.post("/", (req, res) => {
+  const { name, age, gender, notes } = req.body;
+  const ready_to_transfer = req.body.readyForTransfer;
+  const queryText = `INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
+VALUES ($1, $2, $3, $4, $5)`;
+
+  pool
+    .query(queryText, [name, gender, age, ready_to_transfer, notes])
+    .then(() => {
+      console.log("Creating koala");
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log("error in creating koala :>> ", error);
+      res.sendStatus(500);
+    });
+});
 
 // PUT
 
